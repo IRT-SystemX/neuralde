60f9d1219fb5e64795901119f2b2cb9e3e3d060e
Josquin Foulliaron <josquin.foulliaron@irt-systemx.fr>
2025-03-07 11:32:27 +0000

    separated requirement from setup.py and updated req file for python39 and 312

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ def parse_requirements(filename):
 setuptools.setup(
     name="neural_de",
     packages=setuptools.find_packages(),
-    version="1.0.0",
+    version="1.1",
     author="Nelson Fernandez Pinto",
     author_email="nelson.fernandez-pinto@irt-systemx.fr",
     description="Image enhancement library: corruption (noise, meteorological...), removal",
     data_files=[('neural_de', ["neural_de/external/_checksums/checksums.json","neural_de/external/_repositories/external_models_list.yaml"])],
     include_package_data=True,
     license="TODO",
-    install_requires=parse_requirements('requirements.txt')
+    # install_requires=parse_requirements('requirements_python.txt')
 )
 
