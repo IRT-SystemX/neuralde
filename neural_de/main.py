103b8e6fccb7edb6cceec7dfdb16c0e80d134735
Josquin FOULLIARON <josquin.foulliaron@irt-systemx.fr>
2025-03-25 15:27:09 +0100

    corrected main.py for flake8

--- neural_de/main.py
+++ neural_de/main.py
@@ -16,10 +16,11 @@ def main():
     Main script of neural de component:
 
     Args:
-        input_source_path: str - Path to the input source to process. If path represent a single file path. The single file will be \
-             processed. If path represents a directory, all images present in directory will be processed
-        output_target_path: str - Output path where results are stored. If input path is a file, this path represents the output file, \
-            else this is the path to output direcotry
+        input_source_path: str - Path to the input source to process. If path represent a single file path.
+            The single file will be processed. If path represents a directory, all images present in directory will be
+            processed
+        output_target_path: str - Output path where results are stored. If input path is a file, this path represents
+            the output file, else this is the path to output direcotry
         pipeline_file_path: str -  Pipeline Configuration file to use
         output_prefix: str (optionnal) - Output_prefix to add to output images filenames
     """
@@ -65,7 +66,7 @@ def main():
     # Create working path lists of images to process
 
     if directory:
-        print("Input source is a directory",args.input_source_path)
+        print("Input source is a directory", args.input_source_path)
         image_paths_list = glob.glob(args.input_source_path + "/*")
         output_paths_list = [
             args.output_target_path + os.sep + args.output_prefix + x.split(os.sep)[-1]
@@ -75,9 +76,6 @@ def main():
         print("Input source is a file")
         image_paths_list = [args.input_source_path]
         output_paths_list = [args.output_target_path]
-
-    #print("input image list", image_paths_list)
-    #print("output image list", output_paths_list)
 
     # Create output dir if needed
 
@@ -90,17 +88,8 @@ def main():
         print("targetdir for image output : ", target_dir)
         Path(target_dir).mkdir(parents=True, exist_ok=True)
 
-
     # Create list of input batch to pass (interpret files as numpy array of tensors)
     input_images_list = [cv2.imread(x) for x in image_paths_list]
-
-    # print("original images sizes", [x.shape for x in input_images_list])
-
-    # Resizing for test in debug
-    # input_images_list = [cv2.resize(x,(382,416), interpolation= cv2.INTER_LINEAR) for x in input_images_list]
-    # input_images_list = [cv2.resize(x,(381,562), interpolation= cv2.INTER_LINEAR) for x in input_images_list]
-
-    # print("resized images sizes", [x.shape for x in input_images_list])
 
     # Apply transformation pipeline
     input_images_list = [cv2.cvtColor(x, cv2.COLOR_BGR2RGB) for x in input_images_list]
